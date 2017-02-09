7fbb8242008459691182709392782c2a7894a6bd
NikkiArman17 <league@Mac-mini-2.local>
2017-02-08 16:54:21 -0800

    Robot is Boss

---
+++ src/day1/robot/MyFirstProgram.java
@@ -0,0 +1,24 @@
+package day1.robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class MyFirstProgram {
+ public static void main(String[] args) {
+	Robot echoRain=new Robot("batman");
+echoRain.penDown();
+echoRain.move(150);
+echoRain.turn(30);
+echoRain.move(-150);
+echoRain.turn(30);
+echoRain.move(150);
+echoRain.turn(30);
+ }
+
+Robot echoRain=new Robot("batman");
+echoRain.penDown();
+echoRain.setSpeed(10);
+for(int i=0;i<75;i++){
+echoRain.setRandomPenColor();
+echoRain.move(5*i);
+echoRain.turn(360/3);
+echoRain.side(7);}
--- src/day1/robot/Spiral.java
+++ src/day1/robot/Spiral.java
@@ -23,5 +23,13 @@ public class Spiral {
 	
 			// 9. Set the pen width to i
 
-	}
-}
+	
+Robot echoRain=new Robot("batman");
+echoRain.penDown();
+echoRain.setSpeed(10);
+for(int i=0;i<75;i++){
+	echoRain.setPenWidth(i);
+echoRain.setRandomPenColor();
+echoRain.move(5*i);
+echoRain.turn(360/7);
+}}}
